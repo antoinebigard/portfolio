13e488ce328acabc32b71c0059d64fc1b9c1c0b5
Gethi <bigard.antoine@gmail.com>
2020-01-06 09:51:23 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-My quick React portfolio
-
-
---
+++ precache-manifest.47f96ce7d2190137cc5e115dd7451752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea6270cc7fc15de8b434c98fca17a04",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "458bac267d6ac415b76b",
+    "url": "/portfolio/static/css/main.9d858e39.chunk.css"
+  },
+  {
+    "revision": "3e1a1a8b3594bcd404ff",
+    "url": "/portfolio/static/js/2.570d32a2.chunk.js"
+  },
+  {
+    "revision": "3e79f09131ebb0ca203e4fc3830ba8ab",
+    "url": "/portfolio/static/js/2.570d32a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "458bac267d6ac415b76b",
+    "url": "/portfolio/static/js/main.6a39915c.chunk.js"
+  },
+  {
+    "revision": "f5ff1fe24da837620bb5",
+    "url": "/portfolio/static/js/runtime-main.c507fcf5.js"
+  }
+]);

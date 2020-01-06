00a29b701dc8f948e33b64dc3df339c35e742143
Gethi <bigard.antoine@gmail.com>
2020-01-06 11:19:25 +0100

    Updates

--- precache-manifest.4a05ee07ec1c03333f18a29404982dca.js
+++ precache-manifest.4a05ee07ec1c03333f18a29404982dca.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "19cd751580214b9cb7a5d7e864e234d4",
+    "revision": "50d9e75df4f5252b4715d1a484e4c99b",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7f256ce7317ca7677595",
+    "revision": "36ed5d8d7711227a56d8",
     "url": "/portfolio/static/css/main.9d858e39.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.9f702a9a.chunk.js.LICENSE"
   },
   {
-    "revision": "7f256ce7317ca7677595",
-    "url": "/portfolio/static/js/main.87d01134.chunk.js"
+    "revision": "36ed5d8d7711227a56d8",
+    "url": "/portfolio/static/js/main.1466113f.chunk.js"
   },
   {
     "revision": "f5ff1fe24da837620bb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f88ae7cfba4e7bb73df8bf65ccdb4c74.js"
+  "/portfolio/precache-manifest.4a05ee07ec1c03333f18a29404982dca.js"
 );
 
 self.addEventListener('message', (event) => {
